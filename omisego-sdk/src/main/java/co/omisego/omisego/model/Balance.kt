babedev/android-sdk

Implement Parcelize

omisego-sdk/src/main/java/co/omisego/omisego/model/Balance.kt
@@ -7,6 +7,8 @@ package co.omisego.omisego.model
  * Copyright © 2017-2018 OmiseGO. All rights reserved.
  */
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.math.BigDecimal
 import java.math.RoundingMode
 
@@ -16,7 +18,8 @@ import java.math.RoundingMode
  * @param mintedToken The minted token corresponding to the balance
  * @param amount The total amount of minted token available for the current user.
  */
-data class Balance(val amount: BigDecimal, val mintedToken: MintedToken) {
+@Parcelize
+data class Balance(val amount: BigDecimal, val mintedToken: MintedToken) : Parcelable {
 
     /**
      * Helper method that returns an easily readable value of the amount

omisego-sdk/src/main/java/co/omisego/omisego/model/Setting.kt
@@ -1,5 +1,8 @@
 package co.omisego.omisego.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /*
  * OmiseGO
  *
@@ -12,4 +15,5 @@ package co.omisego.omisego.model
  *
  * @param mintedTokens A list of minted tokens available for the provider
  */
-data class Setting(val mintedTokens: List<MintedToken>)
+@Parcelize
+data class Setting(val mintedTokens: List<MintedToken>) : Parcelable

gitter-badger/android-sdk-2

omisego-sdk/src/main/java/co/omisego/omisego/model/Balance.kt
@@ -7,6 +7,8 @@ package co.omisego.omisego.model
  * Copyright © 2017-2018 OmiseGO. All rights reserved.
  */
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.math.BigDecimal
 import java.math.RoundingMode
 
@@ -16,7 +18,8 @@ import java.math.RoundingMode
  * @param token The token corresponding to the balance
  * @param amount The total amount of token available for the current user.
  */
-data class Balance(val amount: BigDecimal, val token: Token) {
+@Parcelize
+data class Balance(val amount: BigDecimal, val token: Token): Parcelable {
 
     /**
      * Helper method that returns an easily readable value of the amount

omisego-sdk/src/main/java/co/omisego/omisego/model/Setting.kt
@@ -1,5 +1,8 @@
 package co.omisego.omisego.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /*
  * OmiseGO
  *
@@ -12,4 +15,5 @@ package co.omisego.omisego.model
  *
  * @param tokens A list of tokens available for the provider
  */
-data class Setting(val tokens: List<Token>)
+@Parcelize
+data class Setting(val tokens: List<Token>) : Parcelable